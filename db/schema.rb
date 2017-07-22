3f6ab55f149057bff0b6a88e276f4f38a6d3d1a9
Betty Mutai <bettymutai00@gmail.com>
2017-07-22 20:39:34 +0300

    Added column brand_id and store_id to table shoes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722144435) do
+ActiveRecord::Schema.define(version: 20170722172401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170722144435) do
     t.integer "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "brand_id"
+    t.integer "store_id"
   end
 
   create_table "stores", force: :cascade do |t|
