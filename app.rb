1730bdc3d26a1f6069a095f5cc0ad94af76cea58
Betty Mutai <bettymutai00@gmail.com>
2017-07-22 18:21:41 +0300

    Added a store form in index to add new stores

--- app.rb
+++ app.rb
@@ -1,3 +1,22 @@
 require("bundler/setup")
   Bundler.require(:default)
   Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file}
+
+  get('/') do
+    @stores = Store.all()
+    erb(:index)
+  end
+
+  post('/stores') do
+    store = params.fetch("store")
+    town = params.fetch("town")
+    street = params.fetch("street")
+    store = Store.new(name: store, town: town, street: street)
+    store.save()
+    erb(:index)
+  end
+
+  get('/stores/:id') do
+    @store = Store.find(params.fetch('id').to_i)
+    erb(:store)
+  end
