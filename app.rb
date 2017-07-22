21cb2525b8aa825375a167f64bf63c4b6f9d355e
Betty Mutai <bettymutai00@gmail.com>
2017-07-22 18:47:50 +0300

    Redireced post routes for store and brand

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require("bundler/setup")
 
   get('/') do
     @stores = Store.all()
+    @brands = Brand.all()
     erb(:index)
   end
 
@@ -12,11 +13,29 @@ require("bundler/setup")
     town = params.fetch("town")
     street = params.fetch("street")
     store = Store.new(name: store, town: town, street: street)
-    store.save()
+    if store.save()
+      redirect('/stores/'.concat(store.id.to_s))
+    else
     erb(:index)
+    end
   end
 
   get('/stores/:id') do
     @store = Store.find(params.fetch('id').to_i)
     erb(:store)
+  end
+
+  post('/brands') do
+    brand = params.fetch("brand")
+    brand = Brand.new(name: brand)
+    if brand.save()
+      redirect('/brands/'.concat(brand.id.to_s))
+    else
+    erb(:index)
+    end
+  end
+
+  get('/brands/:id') do
+    @brand = Brand.find(params.fetch("id").to_i)
+    erb(:brand)
   end
