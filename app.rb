68da52f01e2abaeb38e2137f251e7ba47edb13b4
Betty Mutai <bettymutai00@gmail.com>
2017-07-23 23:08:44 +0300

    Added a patch and delete http method for my store

--- app.rb
+++ app.rb
@@ -31,6 +31,25 @@ require("bundler/setup")
     erb(:store)
   end
 
+  patch('/stores/:id') do
+    @store = Store.find(params.fetch("id").to_i)
+    @store.update(name: params.fetch("store"), town: params.fetch("town"), street: params.fetch("street"))
+    image = params.fetch("image")
+    size = params.fetch("size")
+    cost = params.fetch("cost")
+    shoe = Shoe.find_by_store_id(params.fetch("id").to_i)
+    shoe.update(image: image, size: size, cost: cost)
+    erb(:store)
+  end
+
+  delete('/stores/:id') do
+    @store = Store.find(params.fetch("id").to_i)
+    @shoe = Shoe.find_by_store_id(params.fetch("id").to_i)
+    @shoe.destroy
+    @store.destroy
+    redirect("/")
+  end
+
   post('/brands') do
     brand = Brand.new(name: params.fetch("brand"))
     if brand.save()
