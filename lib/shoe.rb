c0c620d7b4b82daf4785e889db2a58d3bba3c4dc
Betty Mutai <bettymutai00@gmail.com>
2017-07-25 10:23:32 +0300

    Removed the method titlecase because it isn't required when adding a new shoe

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,4 @@
 class Shoe < ActiveRecord::Base
   belongs_to :brand
   belongs_to :store
-
-  validates(:name, :presence => true)
-  before_save(:titlecase_name)
-
- private
-  define_method(:titlecase_name) do
-   self.name=(name().titlecase())
- end
 end
